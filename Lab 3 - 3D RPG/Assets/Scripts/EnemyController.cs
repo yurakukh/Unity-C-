a84b099675b3e6da021cb28f19ccd61840e1ead6
kukharenko.yurii <kukharenko.yura@gmail.com>
2021-04-04 10:25:16 +0300

    Lab3 - 3D RPG Game added

---
+++ Lab 3 - 3D RPG/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    class EnemyController : MonoBehaviour, Interactable
+    {
+        [SerializeField] private float _radius;
+        public float InteractRadius => _radius;
+
+        [SerializeField] private Canvas canvas;
+        [SerializeField] private Button button;
+        [SerializeField] private TMP_Text alert;
+
+        public void Interact()
+        {
+            Time.timeScale = 0;
+
+            alert.text = "Fight for your life!";
+
+            button.onClick.AddListener(OnClickInteract);
+
+            canvas.gameObject.SetActive(true);
+        }
+
+        private void OnClickInteract()
+        {
+            Time.timeScale = 1;
+            canvas.gameObject.SetActive(false);
+
+            button.onClick.RemoveListener(OnClickInteract);
+        }
+    }
+}
---
+++ Lab 3 - 3D RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using Assets.Scripts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+    [SerializeField] private LayerMask objects;
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Animator animator;
+    private Interactable interact;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interact != null)
+        {
+            if (agent.remainingDistance <= interact.InteractRadius)
+            {
+                agent.SetDestination(gameObject.transform.position);
+                interact.Interact();
+                RemoveInteract();
+            }
+        }
+
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            animator.SetBool("Move", false);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+            {
+                agent.SetDestination(hit.point);
+                animator.SetBool("Move", true);
+
+                SetInteract(hit.collider.GetComponent<Interactable>());
+            }
+        }
+    }
+
+    void SetInteract(Interactable interactable)
+    {
+        interact = interactable;
+    }
+
+    void RemoveInteract()
+    {
+        interact = null;
+    }
+}
